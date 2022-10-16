b52e796020d9b5a958388f63ae14e4b055ce9773
tangbao <65097367+deartb@users.noreply.github.com>
2022-10-16 16:13:31 +0800

    modify sort

---
+++ sort/quick_sort.cpp
@@ -0,0 +1,61 @@
+//快速排序
+void quick_sort(int *num, int left, int right)
+{
+    // int size = sizeof(num)/sizeof(*num);
+    if(left > right)
+        return ;
+    // if(left < 0 || right > size-1)
+    //     return ;
+    int i = left;
+    int j = right;
+    int pivot = num[i];
+    
+    while(i < j)
+    {
+        while(num[j] >= pivot && i < j)
+        {
+            j--;
+        }
+        num[i] = num[j];
+        while(num[i] <= pivot && i < j)
+        {
+            i++;
+        }
+        num[j] = num[i];
+    }
+    num[i] = pivot;
+    quick_sort(num, left, i-1);
+    quick_sort(num, i+1, right);
+    
+}
+
+// 快排+vector
+vector<int> quicksort_vec(vector<int> &num)
+{
+    if(2 > num.size())
+        return num;
+    quicksort(num, 0, num.size()-1);
+    return num;
+}
+void quicksort(vector<int> &num, int left, int right)
+{
+    if(left > right)
+        return ;
+    int i = left;
+    int j = right;
+    int pivot = num[i];
+    while (i < j)
+    {
+        while(num[j] >= pivot && i < j){
+            j--;
+        } 
+        num[i] = num[j];
+        while(num[i] < pivot && i < j){
+            i++;
+        }
+        num[j] = num[i];
+    }
+    num[i] = pivot;
+    quicksort(num, left, i - 1);
+    quicksort(num, i + 1, right);
+}
--- sort/sort.cpp
+++ sort/sort.cpp
@@ -1,91 +1,87 @@
-# 快速排序
-
 #include<iostream>
+#include<stdlib.h>
 #include<vector>
 using namespace std;
 
-void quick_sort_1(vector<int> &num, int left, int right);
-void quick_sort_2(vector<int>& num, int left, int right);
+// void quicksort(vector<int> &num, int left, int right);
+void quick_sort(vector<int>& num, int left, int right);
 int partition(vector<int>& num,int left,int right);
 
-//快速排序
-void quick_sort_int(int *num, int left, int right)
+//元素交换
+void swap(int &a, int &b)
 {
-    if(left > right)
-        return ;
-    int i = left;
-    int j = right;
-    int pivot = num[i];
-    
-    while(i < j)
-    {
-        while(num[j] >= pivot && i < j)
-        {
-            j--;
-        }
-        num[i] = num[j];
-        while(num[i] <= pivot && i < j)
-        {
-            i++;
-        }
-        num[j] = num[i];
-    }
-    num[i] = pivot;
-    quick_sort_int(num, left, i-1);
-    quick_sort_int(num, i+1, right);
+    int tmp = a;
+    a = b;
+    b = tmp;
+}
+void swap_a(int &a, int &b)
+{
+    a = a + b;
+    b = a - b;
+    a = a - b;
+}
+void swap_b(int *a, int *b)
+{
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
 }
 
-// 快排+vector
-vector<int> quick_sort_vec_basic(vector<int> &num)
+// 选择排序
+void select_sort(int a[], int size)
 {
-    if(2 > num.size())
-        return num;
-    quick_sort_1(num, 0, num.size()-1);
-    return num;
-}
-void quick_sort_1(vector<int> &num, int left, int right)
-{
-    if(left > right)
-        return ;
-    int i = left;
-    int j = right;
-    int pivot = num[i];
-    while (i < j)
+    int i,j,min;
+    for(i=0; i<size-1; i++)
     {
-        while(num[j] >= pivot && i < j){
-            j--;
-        } 
-        num[i] = num[j];
-        while(num[i] < pivot && i < j){
-            i++;
+        min = i;
+        for(j=i+1; j<size; j++)
+        {
+            if(a[j]<a[min]){
+                min = j;
+            }
         }
-        num[j] = num[i];
+        if(min!=i){
+            a[min] = a[min] + a[i];
+            a[i] = a[min] - a[i];
+            a[min] = a[min] - a[i];
+        }       
     }
-    num[i] = pivot;
-    quick_sort_1(num, left, i - 1);
-    quick_sort_1(num, i + 1, right);
 }
 
-//快排+vector+模块化
-vector<int> quick_sort_vec_full(vector<int>& num)
+//冒泡排序
+void popsort(int num[], int size)
+{
+    int i,j;
+    for(i=0; i<size-1; i++)
+        for(j=i+1; j<size; j++){
+            if(num[i]>num[j]){
+                int tmp = num[i];
+                num[i] = num[j];
+                num[j] = tmp;
+            }
+        }
+}
+
+//快排+vector+分层
+vector<int> quick_sort_vec(vector<int>& num)
 {
     int size = num.size();
     if(2 > size){
         return num;
     }
-    quick_sort_2(num, 0, size -1);
+    quick_sort(num, 0, size -1);
     return num;
 }
 
-void quick_sort_2(vector<int>& num, int left, int right)
+void quick_sort(vector<int>& num, int left, int right)
 {
     if (left > right)
     {
         return ;
     }
     int pivot_idx = partition(num, left, right);
-    quick_sort_2(num, left, pivot_idx - 1);
-    quick_sort_2(num, pivot_idx + 1, right);
+    quick_sort(num, left, pivot_idx - 1);
+    quick_sort(num, pivot_idx + 1, right);
 }
 
 int partition(vector<int>& num,int left,int right)
@@ -130,14 +126,17 @@ void print_vec(const vector<int> &vec)
 
 int main()
 {
-    int v1[] = {5,3,1,2,4,7,6,8,9,10};
-    vector<int> v2 = {3,1,2,4,7,6,8,9};
-    vector<int> v3 = {1,2,4,7,6,8};
+    int a[] = {5,3,1,2,4,7,6,8,9,10};
+    // int b[3] = {5,3,1};
+    // select_sort(a,10);
+    // popsort(b,3);
+    // int length = sizeof(a)/sizeof(*a);
+    // // int length = sizeof(b)/sizeof(*b);
+    // quick_sort(a, 0, length-1);
+    // print(a,length);
+    vector<int> v = {5,3,1,2,4,7,6,8,9,10};
+    vector<int> v2 = quick_sort_vec(v);
+    print_vec(v2);
     
-    quick_sort_int(v1, 0, 9);
-    print(v1, 10);
-    print_vec(quick_sort_vec_basic(v2));
-    print_vec(quick_sort_vec_full(v3));
-
     return 0;
 }
